75dad8903c0aaf58c631ac5d01711eb5ec6a1702
nickolasram <nickolasram@gmail.com>
2023-12-21 16:28:34 -0800

    separated MCP

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import './index.css';
-import Stack  from './components/Stack'
-import On2 from './images/On2.png'
-import On2g from './images/On2.gif'
+import MainContentPanel from './components/MainContentPanel';
+
 
 function App() {
   return (
     <div id="wrapper">
       <section className="banner">
         <h1>Complexity Helper</h1>
+        <a>Exercises</a>
+        <a>Flash Cards</a>
       </section>
-      <section className="sidebar-area">
-        <p>Slow</p>
-        <section className='sidebar'>
-          <Stack />
-        </section>
-        <p>Fast</p>
-      </section>
-      <section className='main'>
-        <section className='graph-wrapper'>
-          <img src={On2} />
-          <p className='graph-wrapper'><em>On^2</em></p>
-        </section>
-        <section className='gif-wrapper'>
-          <img src={On2g}/>
-          <p className='algorithm-desc'>
-            <em>Bubble Sort:</em> is the simplest sorting algorithm 
-            that works by repeatedly 
-            swapping the adjacent 
-            elements if they are in the wrong order. 
-            This algorithm is not suitable for large data 
-            sets as its average and worst-case time complexity is quite high.
-          </p>
-        </section>
-      </section>
+      <MainContentPanel />
     </div>
   );
 }
---
+++ src/components/MainContentPanel.js
@@ -0,0 +1,36 @@
+import On2 from '../images/On2.png'
+import On2g from '../images/On2.gif'
+import Stack from './Stack';
+
+let MainContentPanel = () =>{
+
+    return (
+        <section className='main-content-panel'>
+            <section className="sidebar-area">
+                <p>Slow</p>
+                <section className='sidebar'>
+                    <Stack />
+                </section>
+                <p>Fast</p>
+            </section>
+            <section className='main'>
+                <section className='graph-wrapper'>
+                    <img src={On2} />
+                    <p className='graph-wrapper'><em>On^2</em></p>
+                </section>
+                <section className='gif-wrapper'>
+                    <img src={On2g}/>
+                    <p className='algorithm-desc'>
+                        <em>Bubble Sort:</em> is the simplest sorting algorithm 
+                        that works by repeatedly 
+                        swapping the adjacent 
+                        elements if they are in the wrong order. 
+                        This algorithm is not suitable for large data 
+                        sets as its average and worst-case time complexity is quite high.
+                    </p>
+                </section>
+            </section>
+        </section>
+    );
+}
+export default MainContentPanel;
